57c1ae8805427b3f3644481a5160cb455a6fe587
RGeorge482 <69143183+RGeorge482@users.noreply.github.com>
2022-01-25 12:24:05 +0000

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 // after object destructuring
 const forecast = (lat, lon, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=9a14ba1d5c53f22592c482e9c6e63a8d&query=' + lat + ',' + lon + '&units=m'
+  const url = 'http://api.weatherstack.com/current?access_key=/*your access key*/=' + lat + ',' + lon + '&units=m'
   request({url, json: true}, (error, { body }) => {
     if(error) {
       callback('Unable to connect to weather service', undefined)
