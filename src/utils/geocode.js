45be53addc8416064a98e3dc5d833800bad40dd6
RGeorge482 <69143183+RGeorge482@users.noreply.github.com>
2022-01-25 12:25:15 +0000

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 // after object destructuring
 const geocode = (address, callback) => {
-  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZ2VvcmdlMjQ4IiwiYSI6ImNreXBvdGR3bTBjMWkyb3A3dDVva2FxeTkifQ.i-DhMkXoV-_ufUSmJhstCA&limit=1'
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=/*your access token*/'
   request({url: url, json: true}, (error, { body }) => {
     if(error) {
       callback('Unable to connect to Geolocation service', undefined)
